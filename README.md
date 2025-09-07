b9de4ce43be14095d37651eba19abdcb54ce9c4e
Yoshida.Kazutaka <kyoshida1153@gmail.com>
2025-09-07 12:24:04 +0900

    nextjs.org/learn/pages-router

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+
+参考：[日本一わかりやすいNext.js入門](https://www.youtube.com/watch?v=F77RUjHZEAc&list=PLX8Rsrpnn3IUGEyanrHYGjY1WOzNe7Jd-)
---
+++ components/date.js
@@ -0,0 +1,6 @@
+import { parseISO, format } from 'date-fns';
+
+export default function Date({ dateString }) {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, 'M月 d日, yyyy年')}</time>;
+}
